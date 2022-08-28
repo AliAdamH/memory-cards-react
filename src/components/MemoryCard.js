3b91873496c6cfb1c18ba274cb459a93591ae729
Ali Hassaine <ali.hs@outlook.fr>
2022-08-28 15:32:20 +0000

    Add dynamic card generation

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './MemoryCard.css';
 
 const MemoryCard = (props) => {
-  return <div className="memory-card">This card works !</div>;
+  return (
+    <div className="memory-card" onClick={props.onCardClick}>
+      This card works !
+    </div>
+  );
 };
 
 export { MemoryCard };
--- src/components/MemoryCardContainer.js
+++ src/components/MemoryCardContainer.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MemoryCard } from './MemoryCard';
 import './MemoryCardContainer.css';
 const MemoryCardContainer = () => {
+  const CARDS_COUNT = 12;
   const [cards, setCards] = useState([]);
+
+  const handleClick = () => {
+    console.log('card has been clicked');
+  };
 
   const shuffle = () => {
     setCards((prevCards) => {
@@ -10,18 +15,15 @@ const MemoryCardContainer = () => {
     });
   };
 
-  return (
-    <div className="memory-card-container">
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-    </div>
-  );
+  useEffect(() => {
+    let initial_cards = [];
+    for (let i = 0; i < CARDS_COUNT; i++) {
+      initial_cards.push(<MemoryCard key={i} onCardClick={handleClick} />);
+    }
+    setCards(initial_cards);
+  }, []);
+
+  return <div className="memory-card-container">{cards}</div>;
 };
 
 export { MemoryCardContainer };
